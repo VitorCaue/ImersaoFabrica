46dd2941cb734c2b5cc03f7bfc0e74630f6953b5
Vitor Cauê <103452553+VitorCaue@users.noreply.github.com>
2022-09-12 18:25:39 -0300

    Add files via upload

---
+++ exec.py
@@ -0,0 +1,2 @@
+from modulo_1 import operacao
+print(operacao(1,1))
---
+++ modulo_1.py
@@ -0,0 +1,16 @@
+def operacao(x, y):
+    x = int(input("Digite o valor desejado: "))
+    y = int(input("Digite o valor desejado: "))
+    soma = x + y
+    sub = x - y
+    multi = x * y
+    divisao = x / y
+
+    print((soma))
+    print((sub))
+    print((multi))
+    print(divisao)
+
+    print('Operação realiazada!')
+    return
+
