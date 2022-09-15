02d4944c37670819c15e29a23cd59b6bfcdbe3c6
Vitor Caue <vitorcaue392@gmail.com>
2022-09-15 17:52:57 -0300

    Subindo arquivos

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def chiclete(Request):
+    
+    return render(Request, template_name='blog/home.html')
+
+def jujuba(Request):
+    html = "<html><body><h1> Dê! </h1></body></html>"
+    return HttpResponse(html)
